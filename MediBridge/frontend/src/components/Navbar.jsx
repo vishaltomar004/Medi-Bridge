fa58bca0f4b97f3c6353aab04251cca41662346d
Vishal Tomar <vishaltomar004@gmail.com>
2025-04-15 18:38:10 +0530

    UI part done heading to backend

--- MediBridge/frontend/src/components/Navbar.jsx
+++ MediBridge/frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
   const navigate = useNavigate();
 
-  const [showMenu, setShoMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const [token , setToken] =useState(true);
 
   return (
@@ -47,6 +47,20 @@ const Navbar = () => {
           Create Account
         </button>
 }
+<img onClick={() => setShowMenu(true)}  className ='w-6 md:hidden' src={assets.menu_icon} alt="" />
+{/* {Mobile menu ---------------------} */}
+<div className={` ${showMenu ? 'fixed w-full ' : 'h-0 w-0 '} md:hidden right-0 top-0 bottom-0 z-2 overflow-hidden bg-white transition-all `}>
+  <div className='flex items-center justify-between px-5 py-6 '>
+    <img className='w-36' src={assets.logo} alt="" />
+    <img className='w-7' onClick={ () => setShowMenu(false)} src={assets.cross_icon} alt="" />
+  </div>
+  <ul className='flex flex-col items-center gap-2 text-lg font-medium  mt-5 px-5'>
+    <NavLink  onClick={() => setShowMenu(false)}  to='/'> <p className='px-4 py-2 rounded inline-block' >HOME</p></NavLink>
+    <NavLink  onClick={() => setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block' >ALL DOCTORS</p></NavLink> 
+    <NavLink  onClick={() => setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block' >ABOUT</p></NavLink>
+    <NavLink  className='px-4 py-2 rounded inline-block'  onClick={() => setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block' >CONTACT</p></NavLink>
+  </ul>
+</div>
       </div>
     </div>
   )
