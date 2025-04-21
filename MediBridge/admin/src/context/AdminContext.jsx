a5217995b3a71396c9484da1bef64d2082bbdde0
Vishal Tomar <vishaltomar004@gmail.com>
2025-04-21 12:06:08 +0530

    Done with all doctor ui

--- MediBridge/admin/src/context/AdminContext.jsx
+++ MediBridge/admin/src/context/AdminContext.jsx
@@ -1,18 +1,53 @@
 import { createContext } from "react"
 export const AdminContext = createContext()
 import { useState } from "react"
-
-
+import axios from 'axios'
+import { toast } from 'react-toastify'
 const AdminContextProvider = (props) => {
 
-  const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '' )
+  const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
+  const [doctors, setDoctors] = useState([])
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 
+  const getAllDoctors = async () => {
+    try {
+
+      const { data } = await axios.post(backendUrl + '/api/admin/all-doctors', {}, { headers: { aToken } })
+      if (data.success) {
+        setDoctors(data.doctors)
+        console.log(data.doctors);
+
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const changeAvailability = async (docId) => {
+    try {
+
+      const { data } = await axios.post(backendUrl + '/api/admin/change-availability', { docId }, { headers: { aToken } })
+      if (data.success) {
+        toast.success(data.message);
+        getAllDoctors()
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
   const value = {
     aToken,
     setAToken,
     backendUrl,
+    doctors,
+    getAllDoctors,
+    changeAvailability,
   }
   return (
     <AdminContext.Provider value={value}>
--- MediBridge/frontend/src/App.jsx
+++ MediBridge/frontend/src/App.jsx
@@ -10,10 +10,13 @@ import MyAppointments from './pages/MyAppointments'
 import Appointments from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
  const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer/>
   <Navbar></Navbar>
     <Routes>
 <Route path='/' element ={<Home/>} />
