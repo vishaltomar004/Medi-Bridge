29e9636aa4fd92b4850b96a4e681cc5d96cb9b3e
Vishal Tomar <vishaltomar004@gmail.com>
2025-04-21 12:33:33 +0530

    login api for user Done

--- MediBridge/backend/controllers/userController.js
+++ MediBridge/backend/controllers/userController.js
@@ -48,4 +48,31 @@ res.json({success:true , token})
     }
 }
 
-export {registerUser}
+
+// APi for user Login
+
+const loginUser = async(req, res) => {
+    try {
+        const {email, password} = req.body
+        const user = await userModel.findOne({email})
+
+        if(!user){
+            res.json({success:false , message:'User does not exist'})
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(isMatch){
+            const token = jwt.sign({id:user._id} , process.env.JWT_SECRET)
+
+            res.json({success:true , token})
+        }
+        else {
+            res.json({success :false , message:"Invalid credentials"})
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({success : false , message: error.message });
+    }
+}
+export {registerUser , loginUser}
