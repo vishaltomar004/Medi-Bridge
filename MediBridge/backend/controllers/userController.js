45d5f8a8a0224b42d66ec8141f186e4d3ddb08a9
Vishal Tomar <vishaltomar004@gmail.com>
2025-04-23 16:17:39 +0530

    Appointment cancell button done

--- MediBridge/backend/controllers/userController.js
+++ MediBridge/backend/controllers/userController.js
@@ -2,132 +2,130 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken'
-import {v2 as cloudinary}  from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary'
 import doctorModel from '../models/doctorModel.js';
 import appointmentModel from '../models/appointmentModel.js';
 //Api to register user
 
 const registerUser = async (req, res) => {
     try {
-        
-const {name , email , password} = req.body;
 
-if(!name || !password || ! email)
-{
-    return res.json({success:false , message:"Missing Details"})
-}
+        const { name, email, password } = req.body;
 
-if(!validator.isEmail(email)){
-    return res.json({success:false , message:"Enter a valid email"})
-}
+        if (!name || !password || !email) {
+            return res.json({ success: false, message: "Missing Details" })
+        }
 
-if(password.length <8){
-    return res.json({success:false , message:"Enter a strong password"})
-}
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Enter a valid email" })
+        }
 
-// hashing user password
-const salt = await bcrypt.genSalt(10);
-const hashedPassword = await bcrypt.hash(password , salt);
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Enter a strong password" })
+        }
 
-const userData = {
-    name ,
-    email,
-    password : hashedPassword
-}
+        // hashing user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-const newUser = new userModel(userData)
-const user = await newUser.save();
+        const userData = {
+            name,
+            email,
+            password: hashedPassword
+        }
 
-const token = jwt.sign({id:user._id} , process.env.JWT_SECRET)
+        const newUser = new userModel(userData)
+        const user = await newUser.save();
 
-res.json({success:true , token})
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+
+        res.json({ success: true, token })
 
     } catch (error) {
-        
+
         console.log(error);
-        res.json({success : false , message: error.message });
-        
+        res.json({ success: false, message: error.message });
+
     }
 }
 
 
 // APi for user Login
 
-const loginUser = async(req, res) => {
+const loginUser = async (req, res) => {
     try {
-        const {email, password} = req.body
-        const user = await userModel.findOne({email})
+        const { email, password } = req.body
+        const user = await userModel.findOne({ email })
 
-        if(!user){
-           return res.json({success:false , message:'User does not exist'})
+        if (!user) {
+            return res.json({ success: false, message: 'User does not exist' })
         }
         const isMatch = await bcrypt.compare(password, user.password)
 
-        if(isMatch){
-            const token = jwt.sign({id:user._id} , process.env.JWT_SECRET)
+        if (isMatch) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
 
-            res.json({success:true , token})
+            res.json({ success: true, token })
         }
         else {
-            res.json({success :false , message:"Invalid credentials"})
+            res.json({ success: false, message: "Invalid credentials" })
         }
 
     } catch (error) {
         console.log(error);
-        res.json({success : false , message: error.message });
+        res.json({ success: false, message: error.message });
     }
 }
 
 // API to get user Profile data 
 
 const getProfile = async (req, res) => {
-     try {
-        
-       const userId = req.userId
-    //    console.log(userId);
-       
+    try {
 
-       const userData = await userModel.findById(userId).select('-password')
-
-       res.json({success:true , userData})
+        const userId = req.userId
+        //    console.log(userId);
 
 
+        const userData = await userModel.findById(userId).select('-password')
 
-     } catch (error) {
-         console.log(error);
-        res.json({success : false , message: error.message });
-     }
+        res.json({ success: true, userData })
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 const updateProfile = async (req, res) => {
     try {
-        
-                const { name , phone, address, dob, gender} =req.body
-                const imageFile = req.file
 
-                const userId = req.userId;
+        const { name, phone, address, dob, gender } = req.body
+        const imageFile = req.file
 
-                
-                if(!name || !phone || !dob || !gender){
-                    return res.json({success:false , message :"Data missing"})
-                }
-                await userModel.findByIdAndUpdate(userId , {name , phone , address : JSON.parse(address),dob, gender})
+        const userId = req.userId;
 
-                if(imageFile)
-                {
-                    // upload image
 
-                    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type :'image'})
-                    const imageURL = imageUpload.secure_url
+        if (!name || !phone || !dob || !gender) {
+            return res.json({ success: false, message: "Data missing" })
+        }
+        await userModel.findByIdAndUpdate(userId, { name, phone, address: JSON.parse(address), dob, gender })
 
-                    await userModel.findByIdAndUpdate(userId , {image : imageURL})
-                   
-                }
-                res.json({success : true , message:"Profile Updated"})
+        if (imageFile) {
+            // upload image
+
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: 'image' })
+            const imageURL = imageUpload.secure_url
+
+            await userModel.findByIdAndUpdate(userId, { image: imageURL })
+
+        }
+        res.json({ success: true, message: "Profile Updated" })
 
     } catch (error) {
         console.log(error);
-        res.json({success : false , message: error.message }); 
+        res.json({ success: false, message: error.message });
     }
 }
 // API to book appointment
@@ -135,36 +133,36 @@ const updateProfile = async (req, res) => {
 const bookAppointment = async (req, res) => {
 
     try {
-        
-        const { docId , slotDate , slotTime} = req.body
 
-       const userId = req.userId;
-    //    console.log("userId prashant bhai :  ", userId);
-    console.log("slotDate " , slotDate);
-    console.log("slotTime : " , slotTime);
-       let docData = await doctorModel.findById(docId).select('-password')
-    //    console.log("Received body:", req.body);
-        if(!docData.available){
-            return res.json({success:false , message :"Doctor not available"})
+        const { docId, slotDate, slotTime } = req.body
+
+        const userId = req.userId;
+        //    console.log("userId prashant bhai :  ", userId);
+        console.log("slotDate ", slotDate);
+        console.log("slotTime : ", slotTime);
+        let docData = await doctorModel.findById(docId).select('-password')
+        //    console.log("Received body:", req.body);
+        if (!docData.available) {
+            return res.json({ success: false, message: "Doctor not available" })
         }
 
         const slots_booked = docData.slots_booked
 
         // checking for slot availability
 
-        if(slots_booked[slotDate]){
-            if(slots_booked[slotDate].includes(slotTime)){
-                return res.json({success:false , message :"Slot not available"})
-            }else {
+        if (slots_booked[slotDate]) {
+            if (slots_booked[slotDate].includes(slotTime)) {
+                return res.json({ success: false, message: "Slot not available" })
+            } else {
                 slots_booked[slotDate].push(slotTime)
             }
         } else {
-            slots_booked[slotDate] =[]
+            slots_booked[slotDate] = []
             slots_booked[slotDate].push(slotTime)
         }
 
         const userData = await userModel.findById(userId).select('-password')
-       
+
         delete docData.slots_booked
 
 
@@ -172,50 +170,88 @@ const bookAppointment = async (req, res) => {
 
             userId,
             docId,
-            userData ,
+            userData,
             docData,
-            amount : docData.fees,
+            amount: docData.fees,
             slotTime,
             slotDate,
-            date : Date.now()
+            date: Date.now()
         }
 
         const newAppointment = new appointmentModel(appointmentData)
         await newAppointment.save()
 
-// save new slots data in docData
+        // save new slots data in docData
 
-await doctorModel.findByIdAndUpdate(docId , {slots_booked})
-//   console.log("Slots booked : " + slots_booked[slotDate] , slots_booked[slotTime])
-// console.log("slotDate1 " , slotDate);
-// console.log("slotTime1 : " , slotTime);
-console.log(slots_booked)
+        await doctorModel.findByIdAndUpdate(docId, { slots_booked })
+        //   console.log("Slots booked : " + slots_booked[slotDate] , slots_booked[slotTime])
+        // console.log("slotDate1 " , slotDate);
+        // console.log("slotTime1 : " , slotTime);
+        console.log(slots_booked)
 
 
-res.json({success:true , message:"Appointment booked"})
+        res.json({ success: true, message: "Appointment booked" })
 
     } catch (error) {
-        
+
         console.log(error);
-        res.json({success : false , message : error.message})
-        
+        res.json({ success: false, message: error.message })
+
     }
 }
 
 // Api to get user for frontend my-appointment page
 
-const listAppointment = async (req, res) => 
-{
-  try {
-    
+const listAppointment = async (req, res) => {
+    try {
 
-    const userId = req.userId
-    const appointments = await appointmentModel.find({userId})
 
-    res.json({success:true , appointments})
-  } catch (error) {
-    console.log(error);
-    res.json({success : false , message : error.message})
-  }
+        const userId = req.userId
+        const appointments = await appointmentModel.find({ userId })
+
+        res.json({ success: true, appointments })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
-export {registerUser , loginUser, getProfile , updateProfile, bookAppointment , listAppointment}
+
+// API to cancel appointmene
+
+const cancelAppointment = async (req, res) => {
+
+    try {
+
+        const { userId ,appointmentId } = req.body;
+        // const userId = req.userId;
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        // verify appointmnet user
+
+        if (appointmentId.userId != userId) {
+            return res.json({ success: false, message: 'Unauthorised action for cancelling appointment' })
+
+        }
+
+        await appointmentModel.findByIdAndUpdate(appointmentId, { cancelled: true })
+
+        // releasing doctor slot
+
+        const { docId, slotDate, slotTime } = appointmentData
+        const doctorData = await doctorModel.findById(docId);
+
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] =slots_booked[slotDate].filter(e => e !== slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId , {slots_booked})
+
+        res.json({success:true , message :'Appointment cancelled'})
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment , cancelAppointment }
--- MediBridge/frontend/src/pages/MyAppointments.jsx
+++ MediBridge/frontend/src/pages/MyAppointments.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify'
 
 const MyAppointments = () => {
 
-  const { backendUrl , token } = useContext(AppContext)
+  const { backendUrl , token ,getDoctorsData} = useContext(AppContext)
 
   const [appointments , setAppointments] = useState([])
   const months = [ "", "Jan" , "Feb" ,"Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec"]
@@ -32,6 +32,28 @@ const MyAppointments = () => {
       toast.error(error.message)
       
     }
+  }
+
+  const cancelAppointment = async (appointmentId) => {
+    
+try {
+  // console.log(appointmentId);
+
+  const {data} = await axios.post(backendUrl + '/api/user/cancel-appointment' , {appointmentId} , {headers:{token}})
+  if(data.success){
+    toast.success(data.message);
+    getUserAppointments()
+    getDoctorsData()
+
+  } else {
+    toast.error(data.message)
+  }
+  
+} catch (error) {
+  console.log(error);
+  toast.error(error.message)
+}
+   
   }
   
 useEffect(() => {
@@ -62,8 +84,10 @@ useEffect(() => {
             </div>
             <div></div>
             <div className='flex flex-col gap-2 judtify-end'>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover::text-white transition-all duration-400 '>Pay Online</button>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border  hover:bg-red-600 hover::text-white transition-all duration-400'>Cancel Appointments</button>
+          {!item.cancelled &&  <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover::text-white transition-all duration-400 '>Pay Online</button>}  
+           
+              {!item.cancelled && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border  hover:bg-red-600 hover::text-white transition-all duration-400'>Cancel Appointments</button> } 
+              {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'> Appointment cancelled</button>}
             </div>
           </div>
 
